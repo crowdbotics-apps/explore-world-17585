4308b36f607d1155c8c464f04a4d1290ab2a462b
Crowdbotics <team@crowdbotics.com>
2020-05-30 18:34:20 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen55410', human_name: 'Blank Screen', access_route: 'BlankScreen55410'},
 { name: 'BlankScreen55409', human_name: 'Blank Screen', access_route: 'BlankScreen55409'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen55410Navigator from '../features/BlankScreen55410/navigator';
 import BlankScreen55409Navigator from '../features/BlankScreen55409/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import TutorialNavigator from '../features/Tutorial/navigator';
@@ -22,6 +23,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+BlankScreen55410: { screen: BlankScreen55410Navigator },
 BlankScreen55409: { screen: BlankScreen55409Navigator },
 Messenger: { screen: MessengerNavigator },
 Tutorial: { screen: TutorialNavigator },
